a9c7bc75532480be5554fed8e6dd5006eb97c72e
SM-Wistful <wickedvirtue@yahoo.com>
2018-02-22 17:25:18 -0500

    init from local

---
+++ README.md
@@ -0,0 +1,46 @@
+# Discord-RoleReact
+
+A bot that automatically assigns roles based on message reactions.
+
+![Demo of Bot](https://i.imgur.com/5vxxCDw.gif)
+
+## Features
+
+- Customizable messages, reactions, commands, and roles
+
+- Auto-removes role when user removes a reaction
+
+## Installation
+
+Clone this repository to your local machine.
+
+Open `roleReact.js`, you will need to change most of the settings at the top of the file. The relevant lines look like this: 
+
+```JavaScript
+//Settings!
+const yourID = ""; //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "!createroleressage"
+let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
+const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
+const reactions = ["💻", "🖌", "😃", "🆕"];
+const botToken = ""; /*You'll have to set this yourself; read more
+                     here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
+```
+
+For the bot to work properly, you must:
+
+1.) Fill in the yourID variable; you can get it in any discord channel by typing "\@YOUR_NAME". For me, it would be \@Night.
+
+2.) Adjust the setup command, if you do not like the default one
+
+3.) Adjust the initial message, if you do not like the default one
+
+4.) Adjust the roles to ones that are actually in your server; **make sure the bot's role is high enough on the list in your server's settings that it can assign the them properly**.
+
+5.) Replace the reactions with the ones of your choosing. **Note:** to get the value for the reaction, do NOT simply type `:my_reaction:` and copy the result. You must type `\:my_reaction` with the backslash and copy that result after pressening enter. If done correctly, the reaction should appear smaller than usual.
+
+6.) Set up the bot and get the token. This [Github Wiki Page](here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token) does a better job of expaining it than I ever could. An image of the permissions the bot needs can be [found here](https://i.imgur.com/PFDm3pH.png).
+
+## Conclusion 
+
+I hope you find this bot useful. If you need any help, or find any bugs, use the appropriate forums in this repository to contact me.
---
+++ roleReact.js
@@ -0,0 +1,69 @@
+//Settings!
+const yourID = ""; //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "!createroleressage"
+let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
+const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
+const reactions = ["💻", "🖌", "😃", "🆕"];
+const botToken = ""; /*You'll have to set this yourself; read more
+                     here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
+
+
+//Load up the bot...
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login(botToken);
+
+//If there isn't a reaction for every role, scold the user!
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
+            });
+        }
+    }
+})
+
+
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.messages.fetch(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+       
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != bot.user.id){
+                var roleObj = msg.guild.roles.find('name', role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.roles.add(roleObj)
+                } else {
+                    memberObj.roles.remove(roleObj);
+                }
+            }
+        }
+        });
+ 
+    }   
+});
